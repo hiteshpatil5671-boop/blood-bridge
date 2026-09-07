39ed6b8013251f7c6c54a85d8fa68050f57ce5af
hiteshpatil5671-boop <hiteshpatil5671@gmail.com>
2026-09-07 21:33:58 +0530

    Create Donation model with schema definition

---
+++ models/Donation.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const donationSchema = new mongoose.Schema(
+    {
+        donorId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Donor",
+            required: true
+        },
+
+        donationDate: {
+            type: Date,
+            required: true
+        },
+
+        bloodGroup: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Donation", donationSchema);
