27ede91e0908621d0add3a7c490cc9217e5914da
hiteshpatil5671-boop <hiteshpatil5671@gmail.com>
2026-09-20 21:40:14 +0530

    Fix donorSchema type declaration

--- Donor.js
+++ Donor.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-"const donorSchema = new mongoose.Schema( 
+const  donorSchema = new mongoose.Schema( 
     {
         name: {
-            type: String,
+            type: ,
             required: true,
             trim: true
         },
