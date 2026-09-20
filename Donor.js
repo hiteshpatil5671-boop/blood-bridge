35d52a1b8d083083952efa07bbc18108befd7e88
hiteshpatil5671-boop <hiteshpatil5671@gmail.com>
2026-09-20 21:38:32 +0530

    Fix formatting issues in Donor.js

--- Donor.js
+++ Donor.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const donorSchema = new mongoose.Schema( 
-    {,
+"const donorSchema = new mongoose.Schema( 
+    {
         name: {
             type: String,
             required: true,
